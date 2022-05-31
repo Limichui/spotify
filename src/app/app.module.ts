06314076ed142f88444c2fedc6e52646b429c56e
Limbert Olmos <49576312+Limichui@users.noreply.github.com>
2022-05-31 00:22:17 -0400

    Se implemento el Router Oulet en home-pagecomponent.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FavoritesComponent } from './modules/favorites/favorites.component';
+
 
 @NgModule({ //TODO: Declaraciones, componentes, directivas, pipes
   declarations: [
-    AppComponent,
-    FavoritesComponent,
+    AppComponent
 
   ],
   imports: [ //TODO: Solo se importan otros modulos
