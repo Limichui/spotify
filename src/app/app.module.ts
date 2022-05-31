78c7ec8a924338115c2606cb2453ad78c3f83ad1
Limbert Olmos <49576312+Limichui@users.noreply.github.com>
2022-05-30 23:47:46 -0400

    Se genero sus componentes page para todos los modulos: tracks-page, history-page, favorite-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FavoritesComponent } from './modules/favorites/favorites.component';
 
 @NgModule({ //TODO: Declaraciones, componentes, directivas, pipes
   declarations: [
     AppComponent,
+    FavoritesComponent,
 
   ],
   imports: [ //TODO: Solo se importan otros modulos
