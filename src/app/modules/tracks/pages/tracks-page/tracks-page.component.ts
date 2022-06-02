1cd7c62568f827202c43a2c741f7f46eb59b2f3b
Limbert Olmos <49576312+Limichui@users.noreply.github.com>
2022-06-02 00:28:43 -0400

    Comunicación entre componentes: tracks-pages y section-generic

--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { __importDefault } from 'tslib';
 
 @Component({
   selector: 'app-tracks-page',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tracks-page.component.css']
 })
 export class TracksPageComponent implements OnInit {
+  mockTracksList = [
+    {
+      name: 'BEBE (Oficial)'
+    },
+    {
+      name: 'Getting Over'
+    },
+    {
+      name: 'Snow tha Product II BZRP Music Sessions Shop (feat. Olivia))'
+    }
+  ]
 
   constructor() { }
 
