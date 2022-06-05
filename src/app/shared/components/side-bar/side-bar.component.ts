c1834e54d4f74ed7d05ec6c4c7c5d9308a2e2563
Limbert Olmos <49576312+Limichui@users.noreply.github.com>
2022-06-04 23:11:16 -0400

    Implementación de Rutas (router) en el side-bar

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -28,7 +28,8 @@ export class SideBarComponent implements OnInit {
       {
         name: 'Tu biblioteca',
         icon: 'uil uil-chart',
-        router: ['/', 'favorites']
+        router: ['/', 'favorites'],
+        query: {hola: 'mundo'}
       }
     ]
 
