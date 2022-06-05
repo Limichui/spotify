3981018b9f567804f7dde2835165a925a60d6207
Limbert Olmos <49576312+Limichui@users.noreply.github.com>
2022-06-05 13:47:16 -0400

    Implementación de filtro (pipe) en el play-list-body.component

---
+++ src/app/shared/pipe/order-list.pipe.ts
@@ -0,0 +1,37 @@
+//import { IfStmt } from '@angular/compiler';
+import { Pipe, PipeTransform } from '@angular/core';
+//import { constants } from 'buffer';
+import { TrackModel } from '../../core/models/tracks.model';
+
+@Pipe({
+  name: 'orderList'
+})
+export class OrderListPipe implements PipeTransform {
+
+  transform(value: Array<any>, args: string | null = null, sort:string = 'asc'): TrackModel[] {
+    try{
+      if (args === null){
+        return value
+      }else{
+        const tmpList = value.sort((a, b) => {
+          if (a[args] < b[args]){
+            return -1
+          }
+          else if (a[args] === b[args]){
+            return 0;
+          }
+          else if (a[args] < b[args]){
+            return 1;
+          }
+          return 1
+        });
+        return (sort === 'asc') ? tmpList : tmpList.reverse()
+
+      }
+    } catch (e) {
+      console.log('Error: ¡Algo paso!')
+      return value
+    }
+  }
+
+}
