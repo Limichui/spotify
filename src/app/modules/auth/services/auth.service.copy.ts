40386dd390257e7254f41351f1abfdb4362a3eff
Limbert Olmos <49576312+Limichui@users.noreply.github.com>
2022-06-11 23:27:47 -0400

    Implementación del HTTP Post Login y Cookie

--- src/app/modules/auth/services/auth.service.copy.ts
+++ src/app/modules/auth/services/auth.service.copy.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
 import { Observable } from 'rxjs';
-
+import { tap } from 'rxjs/operators';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ import { Observable } from 'rxjs';
 export class AuthService {
 
   private readonly URL = environment.api
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private cookie: CookieService) { }
 
   sendCredentials(email:string, password: string): Observable<any>{
     const body = {
@@ -18,6 +19,11 @@ export class AuthService {
       password
     }
     return this.http.post(`${this.URL}//auth/login`, body)
-
+    .pipe(
+      tap((responseOK:any) => {
+        const {tokenSession, data } = responseOK
+        this.cookie.set('token_servicio', tokenSession, 4, '/')
+      })
+    )
   }
 }
